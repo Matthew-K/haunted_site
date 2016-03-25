34a63d957c18b88aa6c35ab1978b35612a7025ea
Matthew Kolodziejczak <makolodzi@gmail.com>
2016-03-25 15:24:49 -0400

    Add header and footer partials and Bootstrap

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
+//home page
 app.get('/', function (req, res) {
   res.render('home');
 });
