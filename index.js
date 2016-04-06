9fddea9081dfbab49edde5b63fd761965b6feed9
Matthew Kolodziejczak <makolodzi@gmail.com>
2016-04-06 15:50:14 -0400

    Refactor routes

--- index.js
+++ index.js
@@ -3,137 +3,26 @@ var express 			= require("express"),
 	mongoose 			= require("mongoose"),
 	methodOverride		= require("method-override"),
 	app 				= express(),
-	HauntedPlace 		= require("./models/haunted_place"),
 	seedDB				= require("./seeds"),
+	HauntedPlace 		= require("./models/haunted_place"),
 	Comment				= require("./models/comment");
 
-seedDB();
+var commentRoutes 	= require("./routes/comments"),
+	hauntedRoutes 	= require("./routes/haunted_places"),
+	indexRoutes		= require("./routes/index");
+
 mongoose.connect("mongodb://localhost/haunted_website");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
+app.use("/", indexRoutes);
+app.use("/haunted_places", hauntedRoutes);
+app.use("/haunted_places/:id/comments", commentRoutes);
 
-// HOME PAGE
-app.get("/", function(req, res) {
-  res.render("home");
-});
-
-// INDEX
-app.get("/haunted_places", function(req, res){
-	HauntedPlace.find({}, function(err, haunted_places){
-		if(err){
-			console.log(err);
-		} else {
-			res.render("haunted_places/index", {haunted_places: haunted_places});
-		}
-	});
-});
-
-// NEW
-app.get("/haunted_places/new", function(req, res){
-	res.render("haunted_places/new");
-});
-
-// CREATE
-app.post("/haunted_places", function(req, res){
-	//add new haunted place to haunted_places collection
-	HauntedPlace.create(
-     {name: req.body.name, image: req.body.image, description: req.body.description},
-     function(err, haunted_place){
-      if(err){
-          console.log(err);
-      } else {
-      	//render haunted_places with new haunted place now on the page
-		res.redirect("/haunted_places");
-      }
-    });
-});
-
-// SHOW
-app.get("/haunted_places/:id", function(req, res){
-	HauntedPlace.findById(req.params.id).populate("comments").exec(function (err, haunted_place) {
-		if(err){
-			console.log(err);
-		} else {
-			res.render("haunted_places/show", {haunted_place: haunted_place});
-		}
-	});
-});
-
-// EDIT
-app.get("/haunted_places/:id/edit", function(req, res){
-	HauntedPlace.findById(req.params.id, function(err, haunted_place){
-		if(err){
-			console.log(err);
-		} else {
-			res.render("haunted_places/edit", {haunted_place: haunted_place});
-		}
-	});
-});
-
-// UPDATE
-app.put("/haunted_places/:id", function(req, res){
-	HauntedPlace.findByIdAndUpdate(req.params.id, req.body.haunted_place, function(err, updated_haunted_place){
-		if(err){
-			console.log(err);
-			consolelog("errror");
-			res.redirect("/haunted_places");
-		} else {
-			res.redirect("/haunted_places/" + req.params.id);
-		}
-	});
-});
-
-// DELETE 
-app.delete("/haunted_places/:id", function(req, res){
-	HauntedPlace.findByIdAndRemove(req.params.id, function(err){
-		if(err){
-			console.log(err);
-			res.redirect("/haunted_places");
-		} else {
-			res.redirect("/haunted_places");
-		}
-	});
-});
-
-// ======================
-//     COMMENT ROUTES    
-// ======================
-
-// NEW
-app.get("/haunted_places/:id/comments/new", function(req, res){
-	HauntedPlace.findById(req.params.id, function(err, haunted_place){
-		if(err){
-			console.log(err);
-		} else{
-			res.render("comments/new", {haunted_place: haunted_place});
-		}
-	});
-});
-
-// CREATE
-app.post("/haunted_places/:id/comments", function(req, res){
-	HauntedPlace.findById(req.params.id, function(err, haunted_place){
-		if(err){
-			console.log(err);
-			res.redirect("/haunted_places");
-		} else {
-			Comment.create(req.body.comment, function(err, comment){
-				if(err){
-					console.log(err);
-				} else {
-					haunted_place.comments.push(comment);
-					haunted_place.save();
-					res.redirect("/haunted_places/" + haunted_place._id);
-				}
-			});
-		}
-	});
-});	
-
-
+//Seed the database
+seedDB();
 
 app.listen(3000, function () {
   console.log("Server started");
